d377aa0beb03cf58bbc52df244d78ee4b91769e7
Josh Black <joshblack@us.ibm.com>
2017-11-28 17:31:27 -0600

    chore: add prod config for webpack example

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  bail: true,
   devtool: 'source-map',
   entry: [path.resolve(__dirname, './src/index.js')],
   output: {
@@ -29,39 +31,45 @@ module.exports = {
           // in development "style" loader enables hot editing of CSS.
           {
             test: /\.s?css$/,
-            use: [
-              require.resolve('style-loader'),
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 2,
+            loader: ExtractTextPlugin.extract({
+              fallback: require.resolve('style-loader'),
+              use: [
+                {
+                  loader: require.resolve('css-loader'),
+                  options: {
+                    importLoaders: 2,
+                    sourceMap: true,
+                  },
                 },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    require('autoprefixer')({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
+                {
+                  loader: require.resolve('postcss-loader'),
+                  options: {
+                    // Necessary for external CSS imports to work
+                    // https://github.com/facebookincubator/create-react-app/issues/2677
+                    ident: 'postcss',
+                    plugins: () => [
+                      require('postcss-flexbugs-fixes'),
+                      require('autoprefixer')({
+                        browsers: [
+                          '>1%',
+                          'last 4 versions',
+                          'Firefox ESR',
+                          'not ie < 9', // React doesn't support IE8 anyway
+                        ],
+                        flexbox: 'no-2009',
+                      }),
+                    ],
+                    sourceMap: true,
+                  },
                 },
-              },
-              {
-                loader: require.resolve('sass-loader'),
-                options: {},
-              },
-            ],
+                {
+                  loader: require.resolve('sass-loader'),
+                  options: {
+                    sourceMap: true,
+                  },
+                },
+              ],
+            }),
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
@@ -83,4 +91,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+    new ExtractTextPlugin({
+      filename: 'static/css/[name].[contenthash:8].css',
+    }),
+  ],
 };
