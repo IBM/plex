b38805fc8f0912895e25e600dc7acd4b22b9e9e6
James Y. Rauhut <jamesrauhut@us.ibm.com>
2018-02-16 10:38:55 -0600

    fix(scss): No cyrillic unicode ranges for Sans Condensed yet

--- scripts/data/families.js
+++ scripts/data/families.js
@@ -3,19 +3,23 @@
 const families = [
   {
     type: "Mono",
-    name: "IBM Plex Mono"
+    name: "IBM Plex Mono",
+    unicodes: ["Latin1", "Latin2", "Latin3", "Pi", "Cyrillic"]
   },
   {
     type: "Sans",
-    name: "IBM Plex Sans"
+    name: "IBM Plex Sans",
+    unicodes: ["Latin1", "Latin2", "Latin3", "Pi", "Cyrillic"]
   },
   {
     type: "Sans Condensed",
-    name: "IBM Plex Sans Condensed"
+    name: "IBM Plex Sans Condensed",
+    unicodes: ["Latin1", "Latin2", "Latin3", "Pi"]
   },
   {
     type: "Serif",
-    name: "IBM Plex Serif"
+    name: "IBM Plex Serif",
+    unicodes: ["Latin1", "Latin2", "Latin3", "Pi", "Cyrillic"]
   }
 ];
 
--- scripts/export-scss.js
+++ scripts/export-scss.js
@@ -3,18 +3,18 @@
  * need for each font family, supported weight, and unicode range.
  */
 
-'use strict';
+"use strict";
 
-const fs = require('fs-extra');
-const path = require('path');
-const rimraf = require('rimraf');
+const fs = require("fs-extra");
+const path = require("path");
+const rimraf = require("rimraf");
 
-const families = require('./data/families');
-const { formatFilename, createFontFace } = require('./tools');
-const unicodes = require('./data/unicodes');
-const weights = require('./data/weights');
+const families = require("./data/families");
+const { formatFilename, createFontFace } = require("./tools");
+const unicodes = require("./data/unicodes");
+const weights = require("./data/weights");
 
-const OUTPUT_DIRECTORY = path.resolve(__dirname, '../scss');
+const OUTPUT_DIRECTORY = path.resolve(__dirname, "../scss");
 
 /**
  * The general flow for this is to iterate through families, weights, and
@@ -31,20 +31,22 @@ const filesToWrite = families
         // When mapping over the unicodes, we'll create the actual @font-face
         // declarations and the appropriate file labeled by the unicode's type
         // field.
-        const innerFiles = unicodes.map(unicode => {
-          const filename = formatFilename([
-            family.type,
-            weight.type,
-            weight.variant,
-            `_${unicode.type}.scss`,
-          ]);
+        const innerFiles = unicodes
+          .filter(unicode => family.unicodes.includes(unicode.type))
+          .map(unicode => {
+            const filename = formatFilename([
+              family.type,
+              weight.type,
+              weight.variant,
+              `_${unicode.type}.scss`
+            ]);
 
-          return {
-            filename: `${OUTPUT_DIRECTORY}/${filename.replace(' ', '-')}`,
-            content: createFontFace(filename, family, weight, unicode),
-            unicode,
-          };
-        });
+            return {
+              filename: `${OUTPUT_DIRECTORY}/${filename.replace(" ", "-")}`,
+              content: createFontFace(filename, family, weight, unicode),
+              unicode
+            };
+          });
 
         // Create a helpful `_index.scss` partial that imports all of the
         // unicode files that were generated above.
@@ -52,22 +54,24 @@ const filesToWrite = families
           family.type,
           weight.type,
           weight.variant,
-          '_index.scss',
+          "_index.scss"
         ]);
+
         const order = {
           Cyrillic: 1,
           Pi: 2,
           Latin3: 3,
           Latin2: 4,
-          Latin1: 5,
+          Latin1: 5
         };
         const content = innerFiles
           .sort((a, b) => order[a.unicode.type] - order[b.unicode.type])
+          .filter(({ unicode }) => family.unicodes.includes(unicode.type))
           .map(({ unicode }) => {
             const importPath = formatFilename([unicode.type]);
             return `@import '${importPath}';`;
           })
-          .join('\n');
+          .join("\n");
 
         // We spread all the inner files, since they are valid files that we'll
         // want to create in the future, and then reduce over the whole
@@ -75,11 +79,11 @@ const filesToWrite = families
         return [
           ...innerFiles,
           {
-            filename: `${OUTPUT_DIRECTORY}/${filename.replace(' ', '-')}`,
+            filename: `${OUTPUT_DIRECTORY}/${filename.replace(" ", "-")}`,
             content: `$font-prefix: '..' !default;
 ${content}`,
-            weight,
-          },
+            weight
+          }
         ];
       })
       .filter(Boolean)
@@ -87,22 +91,22 @@ ${content}`,
 
     // Here we'll generate a `_index.scss` partial for a specific font family
     // that includes all the various weight files generated for the font-family.
-    const filename = `${OUTPUT_DIRECTORY}/${family.type.replace(' ', '-')}/_index.scss`;
+    const filename = `${OUTPUT_DIRECTORY}/${family.type.replace(" ", "-")}/_index.scss`;
     const content = files
       .filter(file => file.weight)
       .map(({ weight }) => {
         const importPath = formatFilename([weight.type, weight.variant]);
         return `@import '${importPath}/index';`;
       })
-      .join('\n');
+      .join("\n");
 
     return [
       ...files,
       {
         filename,
         content: `$font-prefix: '..' !default;
-${content}`,
-      },
+${content}`
+      }
     ];
   })
   .reduce((acc, array) => acc.concat(array));
@@ -118,7 +122,7 @@ families.forEach(family => {
 
 // Write all the files that we created above
 filesToWrite.forEach(({ filename, content }) => {
-  fs.outputFileSync(filename, content, 'utf8');
+  fs.outputFileSync(filename, content, "utf8");
 });
 
 // Create partial for all families
@@ -127,7 +131,7 @@ $font-prefix: '..' !default;
 
 ${families
   .map(
-    family => `@import '${family.type.replace(' ', '-').toLowerCase()}/index';`
+    family => `@import '${family.type.replace(" ", "-").toLowerCase()}/index';`
   )
-  .join('\n')}`;
-fs.outputFileSync(`${OUTPUT_DIRECTORY}/ibm-type.scss`, rootPartial, 'utf8');
+  .join("\n")}`;
+fs.outputFileSync(`${OUTPUT_DIRECTORY}/ibm-type.scss`, rootPartial, "utf8");
