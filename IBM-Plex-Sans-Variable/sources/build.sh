851fe49f6acebda9772b09606456e823164f9180
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-04-07 13:41:37 -0500

    feat: add plex sans variable font files

---
+++ IBM-Plex-Sans-Variable/sources/build.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+fontmake -m './IBMPlexSansVar-Roman.designspace' -o variable
+fontmake -m './IBMPlexSansVar-Italic.designspace' -o variable
+echo '* REPORT * generated IBM Plex Sans Var variable fonts'
