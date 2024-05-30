d682c9d01157bc52b7b49afdefadcd1b0019dff1
Pavol Meszaros <pavol.meszaros1@ro.ibm.com>
2024-05-30 11:08:31 +0300

    feat: first pass

--- scripts/compile-css.js
+++ scripts/compile-css.js
@@ -5,19 +5,22 @@ const families = require('./data/families');
 const familiesData = process.env.npm_package_config_family ? families.filter(({ packageName }) => { return packageName === process.env.npm_package_config_family }) : families;
 
 const compile = (file, output) => {
-  const { css: expandedCss } = sass.renderSync({ file });
+
+  const { css: expandedCss } = sass.compile(file);
+  
   fs.outputFileSync(`${output}.css`, expandedCss);
 
-  const { css: minifiedCss } = sass.renderSync({
-    file,
-    outputStyle: 'compressed',
+  const { css: minifiedCss } = sass.compile(file, {
+    style: 'compressed',
   });
   fs.outputFileSync(`${output}.min.css`, minifiedCss);
+
+  fs.removeSync(file);
 };
 
 familiesData.forEach(family => {
 
-  const inputFile = `packages/${family.packageName}/scss/index.scss`;
+  const inputFile = `packages/${family.packageName}/scss/css.scss`;
   const output = `packages/${family.packageName}/css/${family.name.replace(/\s/g, '-').toLowerCase()}`;
 
   compile(inputFile, output);
--- scripts/generate-scss.js
+++ scripts/generate-scss.js
@@ -6,74 +6,22 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const { formatFilename, createFontFace } = require('./tools');
+const { createUnicodes, createTools, createEntryScss, createTemporaryScss, creatWeightScss } = require('./tools');
 const unicodes = require('./data/unicodes');
 const weights = require('./data/weights');
 const families = require('./data/families');
 
 const familiesData = process.env.npm_package_config_family ? families.filter(({ packageName }) => { return packageName === process.env.npm_package_config_family }) : families;
 
-/**
- * The general flow for this is to iterate through families, weights, and
- * unicodes, using them to generate an array of objects that contains
- * `filename` and `content` fields.
- *
- * We then use this array to write all the necessary files that we need to
- * support our use-case.
- */
-const filesToWrite = familiesData
-.map((family, index) => {
+const filesToWrite = [];
 
-  // Generate path
-  const output_dir = path.resolve(__dirname, `../packages/${family.packageName}/scss`);
-  familiesData[index].output_dir = output_dir;
+familiesData.forEach((family, index) => {
 
-  // Don't generate scss if the font doesn't have italic weights / existing weights
-  const filteredWeights = weights.filter(weight => {
+    // Generate path
+    const output_dir = path.resolve(__dirname, `../packages/${family.packageName}/scss`);
+    familiesData[index].output_dir = output_dir;
 
-    const italicWeight = weight.variant === 'Italic' || weight.type === 'Italic';
-
-    if (italicWeight) {
-      return family.hasItalic;
-    }
-
-    if (family.weights) {
-      return family.weights.includes(weight.type);
-    }
-
-    return true;
-  });
-
-  const files = filteredWeights
-  .map(weight => {
-    // When mapping over the unicodes, we'll create the actual @font-face
-    // declarations and the appropriate file labeled by the unicode's type
-    // field.
-    const innerFiles = unicodes
-    .filter(unicode => family.unicodes.includes(unicode.type))
-    .map(unicode => {
-      const filename = formatFilename([
-        weight.type,
-        weight.variant,
-        `_${unicode.type}.scss`,
-      ]);
-      return {
-        filename: `${output_dir}/${filename.split(' ').join('-')}`,
-        content: createFontFace(family, weight, unicode),
-        unicode,
-      };
-    })
-    .filter(Boolean);
-
-    // Create a helpful `_index.scss` partial that imports all of the
-    // unicode files that were generated above.
-
-    const filename = formatFilename([
-      weight.type,
-      weight.variant,
-      '_index.scss',
-    ]);
-
+    // Create sass unicodes and tools helpers
     const order = {
       Cyrillic: 1,
       Pi: 2,
@@ -82,92 +30,69 @@ const filesToWrite = familiesData
       Latin1: 5,
     };
 
-    const contentSplit = innerFiles
-    .sort((a, b) => order[a.unicode.type] - order[b.unicode.type])
-    .filter(({ unicode }) => family.unicodes.includes(unicode.type))
-    .map(({ unicode }) => {
-      const importPath = formatFilename([unicode.type]);
-      return `@import '${importPath}';`;
-    })
-    .join('\n');
+    const familyUnicodes = unicodes.filter(unicode => family.unicodes.includes(unicode.type)).sort((a, b) => order[a.type] - order[b.type]);
+    const hasFamilyUnicodes = familyUnicodes.length;
 
-    const contentWhole = createFontFace(family, weight);
+    if (hasFamilyUnicodes) {
 
-    // We spread all the inner files, since they are valid files that we'll
-    // want to create in the future, and then reduce over the whole
-    // collection to flatten the array entries.
-    // String contentWhole (woff) goes before contentSplit (woff2) so modern
-    // browsers will look for split files.
+      const familyUnicodesFileContent = createUnicodes(familyUnicodes);
 
-    return [
-      ...innerFiles,
-      {
-        hasItalic: family.hasItalic,
-        filename: `${output_dir}/${filename.split(' ').join('-')}`,
-        content: `$font-prefix: '..' !default;\n${contentWhole}\n${contentSplit}`,
-        weight,
-      },
-    ];
-  })
-  .filter(Boolean)
-  .reduce((acc, array) => acc.concat(array), []);
+      filesToWrite.push({
+        filename: `${output_dir}/unicodes/_index.scss`,
+        content: familyUnicodesFileContent
+      });
+    }
 
-  // Here we'll generate a `_index.scss` partial for a specific font family
-  // that includes all the various weight files generated for the font-family.
-  // If a family is split from the core stylesheet, we don't use the underscore
-  // since it isn't a partial
+    const toolsFileContent = createTools(hasFamilyUnicodes);
 
-  //const fontFileRoot = family.preferredName || family.type;
+    filesToWrite.push({
+      filename: `${output_dir}/tools/_index.scss`,
+      content: toolsFileContent
+    });
 
-  /*const filename = `${output_dir}/${fontFileRoot.toLowerCase()
-  .split(' ')
-  .join('-')}/_index.scss`;*/
+    // Don't generate scss if the font doesn't have italic weights / existing weights
+    const filteredWeights = weights.filter(weight => {
 
-  /*const content = files
-  .filter(file => file.weight)
-  .map(({ weight }) => {
-        
-    const importPath = formatFilename([weight.type, weight.variant]);
-    return `@import '${importPath}/index';`;
-  })
-  .join('\n');*/
+      const italicWeight = weight.variant === 'Italic' || weight.type === 'Italic';
 
-  return files;
-})
-.reduce((acc, array) => acc.concat(array));
+      if (italicWeight) {
+        return family.hasItalic;
+      }
+
+      if (family.weights) {
+        return family.weights.includes(weight.type);
+      }
+
+      return true;
+    });
+
+    // Generate main scss file as an entry point
+    const entryScssFileContent = createEntryScss(filteredWeights, hasFamilyUnicodes ? familyUnicodes.map((unicode) => unicode.type) : false);
+    filesToWrite.push({
+      filename: `${output_dir}/index.scss`,
+      content: entryScssFileContent
+    });
+
+    // Generate temporary scss file for css generation
+    const temporaryScssFileContent = createTemporaryScss();
+    filesToWrite.push({
+      filename: `${output_dir}/css.scss`,
+      content: temporaryScssFileContent
+    });
+
+    // Generate partial scss files for weights
+    filteredWeights.forEach(weight => {
+
+      const { partialScssFilename, partialScssFileContent } = creatWeightScss(weight, family, hasFamilyUnicodes ? familyUnicodes : false, output_dir);
+      filesToWrite.push({
+        filename: partialScssFilename,
+        content: partialScssFileContent
+      });
+    });
+});
 
 // Write all the files that we created above
 filesToWrite.forEach(({ filename, content }) => {
 
   fs.outputFileSync(filename, content, 'utf8');
-});
-
-familiesData.forEach(family => {
-
-  const output_dir = family.output_dir;
-
-  const weightStrings = 
-  weights.filter(weight => {
-
-    const italicWeight = weight.variant === 'Italic' || weight.type === 'Italic';
-
-    if (italicWeight) {
-      return family.hasItalic;
-    }
-
-    if (family.weights) {
-      return family.weights.includes(weight.type);
-    }
-
-    return true;
-  })
-  .map((weight) => {
-
-    return weight.variant === 'Italic' ? `@import '${weight.type.toLowerCase()}/italic/index';` : `@import '${weight.type.toLowerCase()}/index';`;
-  });
-
-  // Create partial for all families
-  const rootPartial = `$font-prefix: '..' !default;\n\n` + weightStrings.join('\n');
-
-  fs.outputFileSync(`${output_dir}/index.scss`, rootPartial, 'utf8');
 });
