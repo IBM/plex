34cadd4234d443b041e7e109a4699ed508fd2b9d
Jesus Medina IBM <jesus.medina@ibm.com>
2025-12-04 08:56:53 -0300

    IBM Plex Sans Condensed V3.0 - 2025-09-23 (#681)
    * chore: test commit
    
    * chore: test commit
    
    * feat(scripts): update build scripts to properly handle unicode generation
    
    Co-authored-by: Pavol Meszaros <54019675+Palke@users.noreply.github.com>
    
    * feat(plex-sans-condensed): update to v3.0
    
    ---------
    
    Co-authored-by: Leonardo da Costa <leocosta@ibm.com>
    Co-authored-by: Pavol Meszaros <54019675+Palke@users.noreply.github.com>

---
+++ scripts/families.js
@@ -0,0 +1,114 @@
+const koreanUnicodeRanges = require('./unicodes/korean');
+const japaneseUnicodeRanges = require('./unicodes/japanese');
+const chinesetcUnicodeRanges = require('./unicodes/chinesetc');
+const chinesescUnicodeRanges = require('./unicodes/chinesesc');
+const condensedUnicodeRanges = require('./unicodes/condensed');
+
+const families = [
+  {
+    type: 'Mono',
+    name: 'IBM Plex Mono',
+    packageName: 'plex-mono',
+    hasItalic: true,
+    unicodes: ['Latin1', 'Latin2', 'Latin3', 'Pi', 'Cyrillic'],
+  },
+  {
+    type: 'Sans',
+    name: 'IBM Plex Sans',
+    packageName: 'plex-sans',
+    hasItalic: true,
+    unicodes: ['Latin1', 'Latin2', 'Latin3', 'Pi', 'Cyrillic', 'Greek'],
+  },
+  {
+    type: 'Sans Cond',
+    preferredName: 'Sans Condensed',
+    hasItalic: true,
+    name: 'IBM Plex Sans Condensed',
+    packageName: 'plex-sans-condensed',
+    unicodes: condensedUnicodeRanges.map(({ type }) => type)
+  },
+  {
+    type: 'Sans Hebrew',
+    name: 'IBM Plex Sans Hebrew',
+    packageName: 'plex-sans-hebrew',
+    hasItalic: false,
+    unicodes: [],
+  },
+  {
+    type: 'Sans Devanagari',
+    name: 'IBM Plex Sans Devanagari',
+    packageName: 'plex-sans-devanagari',
+    hasItalic: false,
+    unicodes: [],
+  },
+  {
+    type: 'Sans Thai',
+    name: 'IBM Plex Sans Thai',
+    packageName: 'plex-sans-thai',
+    hasItalic: false,
+    unicodes: [],
+  },
+  {
+    type: 'Sans Thai Looped',
+    name: 'IBM Plex Sans Thai Looped',
+    packageName: 'plex-sans-thai-looped',
+    hasItalic: false,
+    unicodes: [],
+  },
+  {
+    type: 'Serif',
+    name: 'IBM Plex Serif',
+    packageName: 'plex-serif',
+    hasItalic: true,
+    unicodes: ['Latin1', 'Latin2', 'Latin3', 'Pi', 'Cyrillic'],
+  },
+  {
+    type: 'Sans Arabic',
+    name: 'IBM Plex Sans Arabic',
+    packageName: 'plex-sans-arabic',
+    hasItalic: false,
+    unicodes: [],
+  },
+  {
+    type: 'Sans KR',
+    name: 'IBM Plex Sans KR',
+    packageName: 'plex-sans-kr',
+    hinted: true,
+    hasItalic: false,
+    unicodes: koreanUnicodeRanges.map(({ type }) => type),
+  },
+  {
+    type: 'Sans JP',
+    name: 'IBM Plex Sans JP',
+    packageName: 'plex-sans-jp',
+    hinted: true,
+    hasItalic: false,
+    unicodes: japaneseUnicodeRanges.map(({ type }) => type),
+  },
+  {
+    type: 'Sans TC',
+    name: 'IBM Plex Sans TC',
+    packageName: 'plex-sans-tc',
+    hinted: true,
+    hasItalic: false,
+    unicodes: chinesetcUnicodeRanges.map(({ type }) => type),
+  },
+  {
+    type: 'Sans SC',
+    name: 'IBM Plex Sans SC',
+    packageName: 'plex-sans-sc',
+    hinted: true,
+    hasItalic: false,
+    unicodes: chinesescUnicodeRanges.map(({ type }) => type),
+  },
+  {
+    type: 'Math',
+    name: 'IBM Plex Math',
+    packageName: 'plex-math',
+    hasItalic: false,
+    unicodes: [],
+    weights: ['Regular'],
+  },
+];
+
+module.exports = families;
--- scripts/parse-unicodes.js
+++ scripts/parse-unicodes.js
@@ -2,13 +2,16 @@
  * `scripts/parse-unicodes.js` is used to help parse unicodes from one of the provided CSS file and output file ready to be used in unicodes folder
  *  usage: yarn unicodes -i {LANGCODE} -p /packages/{package-name}/fonts/split/woff/hinted/{css-input-filename}.css -f {output-filename}.js
  *  example: yarn unicodes -i TC -p /packages/plex-sans-tc/fonts/split/woff/hinted/IBMPlexSansTC-Bold.css -f chinesetc.js
+ *  
+ *  usage for base type set: yarn unicodes -p /packages/{package-name}/fonts/split/woff/{css-input-filename}.css -f {output-filename}.js
+ *  example: yarn unicodes -p /packages/plex-sans-condensed/fonts/split/woff/IBMPlexSansCondensed-Bold.css -f condensed.js
  */
 
 const fs = require('fs-extra');
 const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 
-const subsetRegex = /\/\* Subset: (\d+) \*\//;
+const subsetRegex = /\/\* Subset: (.+) \*\//;
 const unicodeRangeRegex = /unicode-range: (.*?)\n/;
 
 const outputDir = path.resolve(__dirname, `./data/unicodes`);
@@ -17,23 +20,35 @@ const { i, p, f } = argv;
 let collection = `module.exports = [`;
 
 if (p && f && f.includes('.js')) {
-
   const cssPath = path.resolve(__dirname, `../${p}`);
   const cssContent = fs.readFileSync(cssPath).toString();
   const definitionMatches = cssContent.match(/\/*((.|\n)*?)\}\n/gm);
 
-  definitionMatches.forEach((definition) => {
-
+  definitionMatches.forEach(definition => {
     const subsetMatches = definition.match(subsetRegex);
     const unicodeRangeMatches = definition.match(unicodeRangeRegex);
 
-    if (subsetMatches && subsetMatches[1] && unicodeRangeMatches && unicodeRangeMatches[1]) {
-
-      collection += 
-`{
-  type: '${i.toUpperCase()}-${subsetMatches[1]}',
-  characters: [${unicodeRangeMatches[1].split(',').map((item) => `'${item.trim()}'`)}]
-},\n`;
+    if (
+      subsetMatches &&
+      subsetMatches[1] &&
+      unicodeRangeMatches &&
+      unicodeRangeMatches[1]
+    ) {
+      if (i === undefined) {
+        collection += `{
+          type: '*${subsetMatches[1]}',
+          characters: [${unicodeRangeMatches[1]
+            .split(',')
+            .map(item => `'${item.trim()}'`)}]
+        },\n`;
+      } else {
+        collection += `{
+          type: '${i.toUpperCase()}-${subsetMatches[1]}',
+          characters: [${unicodeRangeMatches[1]
+            .split(',')
+            .map(item => `'${item.trim()}'`)}]
+        },\n`;
+      }
     }
   });
 
@@ -42,9 +57,9 @@ if (p && f && f.includes('.js')) {
   fs.outputFileSync(`${outputDir}/${f}`, collection, 'utf8');
 
   console.log(`File written to: ${outputDir}/${f}`);
-
 } else {
-
-  console.log('Missing one of the arguments [-i XY -p /path/to/file.css -f filename.js]');
+  console.log(
+    'Missing one of the arguments [-i XY -p /path/to/file.css -f filename.js]'
+  );
   process.exit(1);
 }
